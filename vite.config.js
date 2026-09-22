db6fd547a73d582f662d91aab420ec734f2dbf3f
Anjali-Kummari <Anjali.Kummari@valuemomentum.com>
2026-09-22 19:40:16 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+ 
 export default defineConfig({
   plugins: [react()],
+ 
   server: {
     proxy: {
       '/aem': {
-        target: 'https://publish-p221102-e2272119.adobeaemcloud.com',
+        target: 'http://dxpvm.eastus.cloudapp.azure.com:4503',
         changeOrigin: true,
-        secure: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/aem/, ''),
       },
     },
